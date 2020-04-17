3c774987f603a554896c9d091257ab9cf4250edf
Naveen Purmani <pnavinreddy1@gmail.com>
2020-04-17 00:25:59 -0400

    Config server setup

---
+++ src/test/java/com/configserver/webservice/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configserver.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
